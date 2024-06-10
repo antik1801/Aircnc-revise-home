43a0ef5c689aed0aa025e06f823fad0cabcd982d
Gazi Ehsanul Haque <antik.edu@gmail.com>
2024-06-10 11:56:35 +0600

    main focused

--- client/src/components/Shared/Logo.jsx
+++ client/src/components/Shared/Logo.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import logoImg from "../../assets/images/logo.png"
-
+import { Link } from "react-router-dom"
 const Logo = () => {
   return (
-    <img src={logoImg} alt="logo" width="100" height="100"/>
-  )
+    <Link to="/" className='hidden md:block'> 
+     <img src={logoImg} alt="logo" width="100" height="100"/>
+     </Link>
+)
 }
 
 export default Logo
