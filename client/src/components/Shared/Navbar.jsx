c68c87b7f2ead83935636b1329ad8b6d08b900f5
Gazi Ehsanul Haque <antik.edu@gmail.com>
2024-06-10 10:56:57 +0600

    main innsest

--- client/src/components/Shared/Navbar.jsx
+++ client/src/components/Shared/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Container from './Container'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='fixed bg-white z-10 shadow-sm'>
+      <div className='py-4 border-b-[1px]'>
+        <Container>
+          <div className='flex flex-row justify-between items-center gap-3 md:gap-0'>
+            <div>logo</div>
+            <div>search</div>
+            <div>menu</div>
+          </div>
+        </Container>
+      </div>
     </div>
   )
 }
