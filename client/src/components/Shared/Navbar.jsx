5f5c0ae01293d279cd8dcf23159394a6efb74c38
Gazi Ehsanul Haque <antik.edu@gmail.com>
2024-06-10 11:15:40 +0600

    main

--- client/src/components/Shared/Navbar.jsx
+++ client/src/components/Shared/Navbar.jsx
@@ -3,10 +3,10 @@ import Container from './Container'
 
 const Navbar = () => {
   return (
-    <div className='fixed bg-white z-10 shadow-sm'>
+    <div className='fixed w-full  bg-white z-10 shadow-sm'>
       <div className='py-4 border-b-[1px]'>
         <Container>
-          <div className='flex flex-row justify-between items-center gap-3 md:gap-0'>
+          <div className='flex flex-row justify-between items-center '>
             <div>logo</div>
             <div>search</div>
             <div>menu</div>
