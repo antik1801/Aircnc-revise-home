a81cafc1941bbfde8bd49e51ecdce8fa14eb92f5
antik1801 <antik.edu@gmail.com>
2024-06-13 17:49:14 +0600

    main frame

--- client/src/components/Shared/MenuDropdown.jsx
+++ client/src/components/Shared/MenuDropdown.jsx
@@ -13,9 +13,11 @@ const MenuDropdown = () => {
   return (
     <div className='relative'>
       <div className='flex flex-row items-center gap-3'>
+        {/* AirCNC your home button */}
         <div className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'>
           AirCNC your home
         </div>
+        {/* Dropdown Button */}
         <div
           onClick={toggleOpen}
           className='p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'
